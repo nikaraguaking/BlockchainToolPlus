cbf1b84e0800ffd061466c2a085ecca0ca6443b8
nikaraguaking <nikaraguaking@proton.me>
2025-10-27 19:53:49 +0100

    Add SurveyXAddresses.ts

---
+++ surveyx-frontend/abi/SurveyXAddresses.ts
@@ -0,0 +1,12 @@
+export const SurveyXAddresses = {
+  "31337": {
+    "address": "0xc11cf49037a020a712C5d51bDF40A2b7ffB184C2",
+    "chainId": 31337,
+    "chainName": "localhost"
+  },
+  "11155111": {
+    "address": "0xAa73D10AA6D3D3eA95F1d6798924021591748946",
+    "chainId": 11155111,
+    "chainName": "sepolia"
+  }
+} as const;
